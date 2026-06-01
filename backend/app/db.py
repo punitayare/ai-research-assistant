6146cac7a05bc5d730b71962310b863e276d9cae
punitayare722 <punit.235857101@vcet.edu.in>
2026-06-01 20:12:22 +0530

    updated requiremnts.txt

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,11 +1,21 @@
-import psycopg2
+import os
 
+import psycopg2
+'''
 conn = psycopg2.connect(
     host="localhost",
     database="ai-research-assistant",
     user="postgres",
     password="new_password123",
     port=5432
+)'''
+
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT", 5432)
 )
 
 cursor = conn.cursor()
